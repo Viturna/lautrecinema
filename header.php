b0c7a262bfbfa3967e26509877207a36b4b8b297
Viturna <viturnapro@gmail.com>
2024-03-13 23:19:50 +0100

    responsive index

--- header.php
+++ header.php
@@ -1,10 +1,24 @@
 <header class="grid">
   <img src="./src/image/logo-red.svg" alt="">
   <nav>
-    <ul>
+    <ul class="menu">
       <li>A propos</li>
       <li>Nos épisodes</li>
       <li>Autre page</li>
     </ul>
+    <div class="burger-menu">
+      <div class="burger"></div>
+    </div>
   </nav>
 </header>
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const menu = document.querySelector('.menu');
+
+  burgerMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+});
+
+</script>
