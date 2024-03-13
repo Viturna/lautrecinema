17d7079cb9831f5e4058f6632eaf664e3ce74a0c
Viturna <viturnapro@gmail.com>
2024-03-13 22:40:26 +0100

    contenu et style desktop index.php

--- index.php
+++ index.php
@@ -1,12 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="./src/css/style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="./src/css/reset.css" rel="stylesheet">
+  <link href="./src/css/style.css" rel="stylesheet">
+
 </head>
+
 <body>
-    <?php include_once './header.php';?>
+  <?php include_once './header.php'; ?>
+  <main class="grid">
+    <div class="top-a-propos">
+      <h1>L'au<span class="snell red">t</span>re <br><span class="snell red">c</span>inéma</h1>
+      <div class="our-goal">
+        <h2>Notre but</h2>
+        <p>Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum.</p>
+        <a href="#" class="button">Découvrir les épisodes</a>
+      </div>
+    </div>
+    <div class="our-team">
+      <h2>Notre équipe</h2>
+      <div class="box-profil">
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Riquier <br> Thomas</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Cabaret <br> Line</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Garbuïo--Binaud <br> Eva</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Juhel <br> Alexis</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Quéméré <br> Edgar</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Merlaud <br> Thomas</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Corberand <br> Antoine</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Cellier <br> Elyser</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Tighidet <br> Billel</p>
+        </div>
+        <div class="profil">
+          <img class="profil-img" src="./src/image/profil-img.jpg" alt="">
+          <p class="name">Valton <br> Gabriel</p>
+        </div>
+      </div>
+    </div>
+
+  </main>
 </body>
+
 </html>
