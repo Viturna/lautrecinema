27f1737db89a3c0b55083bd6115c0433ccfcb1ea
Viturna <viturnapro@gmail.com>
2024-03-13 20:11:33 +0100

    init scss

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+  <img src="./src/image/logo-red.svg" alt="">
+  <nav>
+    <ul>
+      <li>A propos</li>
+      <li>Nos épisodes</li>
+      <li>Autre page</li>
+    </ul>
+  </nav>
+</header>
