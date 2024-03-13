caa5977d2f7ddd5b8bb19825cd12cc599f98a265
LineCab <line.cabaret@mmibordeaux.com>
2024-03-13 23:19:33 +0100

    page épisodes

--- videos.php
+++ videos.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="./src/css/style.css" rel="stylesheet">
+    <link href="./src/css/videos.css" rel="stylesheet">
 </head>
 <body>
     <?php 
@@ -12,43 +13,55 @@
     require_once './config.php';
 
     // Exécuter une requête SELECT
-    $videos = mysqli_query($connexion, "SELECT * FROM video ORDER BY dateAjoutVid LIMIT 1, 15");
     $recents = mysqli_query($connexion, "SELECT * FROM video ORDER BY dateAjoutVid LIMIT 1");
     ?>
 
     <h2>Nouveauté</h2>
-    <div class="card">
-        <?php
-        foreach ($recents as $recent){
-            $title = $recent["titleVid"];
-            $img = $recent["imgVid"]; 
-            $sources = $recent["sourcesVid"];
-            $durée = $recent["timeVid"]; 
-        } 
-        ?>
-
-    </div>
-    <h2>Nos épisodes</h2>
-
     <?php
-    foreach ($videos as $video){
-        $title = $video["titleVid"];
-        $img = $video["imgVid"]; 
-        $sources = $video["sourcesVid"];
-        $durée = $video["timeVid"];
+    foreach ($recents as $recent){
+        $title = $recent["titleVid"];
+        $img = $recent["imgVid"]; 
+        $sources = $recent["sourcesVid"];
+        $durée = $recent["timeVid"];
     ?>
 
-        <div class="card">
-            <img src="<?php echo './src/image/' . $img ?>" alt="">
+        <div class="card card-red">
+            <div class="card-img"><img src="<?php echo './src/image/' . $img ?>" alt="vignette de l'épisode"></div>
             <div class="col">
                 <h3><?php echo $title ?></h3>
                 <p><?php echo $durée ?></p>
             </div>
-            <img src="./src/image/icon-play-white.svg" alt="">
+            <img class="icon-play" src="./src/image/icon-play-white.svg" alt="">
+        </div>
+
+    <?php
+    } 
+    ?>
+
+    <h2>Nos épisodes</h2>
+
+    <?php
+    $videos = mysqli_query($connexion, "SELECT * FROM video ORDER BY dateAjoutVid LIMIT 1, 15");
+
+    foreach ($videos as $video){
+        $title = $video["titleVid"];
+        $img = $video["imgVid"]; 
+        $sources = $video["sourcesVid"];
+        $duree = $video["timeVid"];
+    ?>
+
+        <div class="card">
+            <div class="card-img"><img src="<?php echo './src/image/' . $img ?>" alt="vignette de l'épisode"></div>
+            <div class="col">
+                <h3><?php echo $title ?></h3>
+                <p><?php echo $duree ?></p>
+            </div>
+            <img class="icon-play" src="./src/image/icon-play-red.svg" alt="">
         </div>
         
     <?php
     }
     ?>
+
 </body>
 </html>
