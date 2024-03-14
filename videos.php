945902796a24ae361b5e36bdec7079b7288db792
LineCab <line.cabaret@mmibordeaux.com>
2024-03-14 14:38:26 +0100

    video / episode

--- videos.php
+++ videos.php
@@ -51,23 +51,23 @@
       $videos = mysqli_query($connexion, "SELECT * FROM video ORDER BY dateAjoutVid LIMIT 1, 15");
 
       foreach ($videos as $video) {
+        $id = $video["idVid"];
         $title = $video["titleVid"];
         $img = $video["imgVid"];
-        $sources = $video["sourcesVid"];
         $duree = $video["timeVid"];
       ?>
-
-        <div class="card">
-          <div class="card-img"><img src="<?php echo './src/image/' . $img ?>" alt="vignette de l'épisode"></div>
-          <div class="col">
-            <h3><?php echo $title ?></h3>
-            <p><?php echo $duree ?></p>
+        <a href="<?php echo './episodes/episode' . $id . '.php'; ?>">
+          <div class="card">
+            <div class="card-img"><img src="<?php echo './src/image/' . $img ?>" alt="vignette de l'épisode"></div>
+            <div class="col">
+              <h3><?php echo $title ?></h3>
+              <p><?php echo $duree ?></p>
+            </div>
+            <svg class="icon-play" width="37" height="46" viewBox="0 0 37 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 3.72606V42.2739C0 45.2134 3.13079 46.9994 5.54187 45.3994L34.8346 26.1255C37.0657 24.6744 37.0657 21.3256 34.8346 19.8373L5.54187 0.60056C3.13079 -0.9994 0 0.786602 0 3.72606Z" fill="white" />
+            </svg>
           </div>
-          <svg class="icon-play" width="37" height="46" viewBox="0 0 37 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 3.72606V42.2739C0 45.2134 3.13079 46.9994 5.54187 45.3994L34.8346 26.1255C37.0657 24.6744 37.0657 21.3256 34.8346 19.8373L5.54187 0.60056C3.13079 -0.9994 0 0.786602 0 3.72606Z" fill="white" />
-          </svg>
-
-        </div>
+        </a>
 
       <?php
       }
