c499f3be47ac827801d929f7228c1efcf8eadb33
LineCab <line.cabaret@mmibordeaux.com>
2024-03-20 21:39:42 +0100

    code en dur html

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-  <img class="footer-logo" src="/src/image/logo-white.svg" fill="#FFF">
-  <p>Projet étudiant MMI Bordeaux - <a>Mentions légales</a> - <a>Politique de confidentialité</a></p>
-  <p>Showtime</p>
-</footer>
